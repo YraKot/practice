68132aa267123d8f7a9371fc7d5aeb4a33deab99
Yr <y.kitsera@gmail.com>
2019-09-11 17:57:36 +0300

    add dram-kit app

---
+++ dram-kit/script.js
@@ -0,0 +1,24 @@
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return; //stop 
+    
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');   
+}
+
+function removeTransitioned(e) {
+    if( e.propertyName !== 'transform' ) return; //search property    
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach( key => {
+    key.addEventListener('transitionend', removeTransitioned);
+});
+
+window.addEventListener('keydown',  playSound);
