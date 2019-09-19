4b63a29f2167c83ff627c5308afcbe02995fd85c
Yra Kot <yrakot@Yras-MacBook-Air.local>
2019-09-19 23:17:45 +0300

    add slide-in-scroll app

---
+++ slide-in-scroll/script.js
@@ -0,0 +1,35 @@
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const slideImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    slideImages.forEach(slideImage => {
+        const sliceInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+
+        const imageBottom = slideImage.offsetTop + slideImage.height;
+        const isHalfShown = sliceInAt > slideImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if( isHalfShown && isNotScrolledPast ) {
+            slideImage.classList.add('active');
+        } else {
+            slideImage.classList.remove('active');
+        }
+    })
+    console.log(e);
+}
+
+window.addEventListener('scroll', debounce(checkSlide) );
